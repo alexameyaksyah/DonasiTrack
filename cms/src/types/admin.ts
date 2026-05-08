e0214558c430ef5321ddbc29aa3105169714ed4d
NaswaNovellia-23312014 <naswa_novellia@teknokrat.ac.id>
2026-05-08 14:16:27 +0700

    Memisahkan definisi tipe data menu admin ke folder types

---
+++ cms/src/types/admin.ts
@@ -0,0 +1,5 @@
+export type AdminMenuKey = "dashboard" | "campaigns" | "verification" | "logistics" | "users" | "report" | "profile";
+
+export type AdminConsoleSidebarProps = {
+  active: AdminMenuKey;
+};
